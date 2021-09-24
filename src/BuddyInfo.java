e14b54909160a09e05e2fbe965e5d35ad4696325
Andris Skuja <andrisskuja@cmail.carleton.ca>
2021-09-24 10:16:50 -0400

    lab2 second commit

---
+++ src/BuddyInfo.java
@@ -0,0 +1,39 @@
+public class BuddyInfo {
+
+
+    private String address;
+    private int number;
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public BuddyInfo(String name, String address, int number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public BuddyInfo() {
+        this("name","address",0);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        BuddyInfo test = new BuddyInfo("Homer","idk",6);
+        String name = test.getName();
+        System.out.println("Hello " + name);
+    }
+}
